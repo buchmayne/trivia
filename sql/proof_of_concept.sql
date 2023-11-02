b06136cf345fe391bb7f5fb59dc5eea872cc4a43
Marley Buchman <michaelmbuchman@gmail.com>
2023-11-01 17:05:07 -0700

    worked my way through the choice set

--- sql/proof_of_concept.sql
+++ sql/proof_of_concept.sql
@@ -7,6 +7,7 @@
 
 drop table if exists questions;
 drop table if exists single_slide_multiple_choice_no_answer_bank;
+drop table if exists choices_single_slide_multiple_choice_no_answer_bank;
 
 create table questions (
 	id SERIAL primary key,
@@ -95,4 +96,162 @@ update single_slide_multiple_choice_no_answer_bank
 				
 
 
-select * from single_slide_multiple_choice_no_answer_bank ssmcnab ;
+create table choices_single_slide_multiple_choice_no_answer_bank as (
+	select 
+		ssmcnab.id,
+		ssmcnab.title,
+		ssmcnab.category,
+		ssmcnab.game_id,
+		ssmcnab.question_number,
+		ssmcnab.points,
+		ssmcnab.points_application,
+		ssmcnab.n_options,
+		case 
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 1 then 'A'
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 2 then 'B'
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 3 then 'C'
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 4 then 'D'
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 5 then 'E'
+			when row_number() over (partition by ssmcnab.id order by ssmcnab.question_number) = 6 then 'F'
+			else 'UNKNOWN'
+		end as choice_id
+	from single_slide_multiple_choice_no_answer_bank ssmcnab
+	join generate_series(1, ssmcnab.n_options) on true
+);
+
+
+alter table choices_single_slide_multiple_choice_no_answer_bank
+add column choice_text varchar,
+add column choice_answer varchar;
+
+-- Question 1
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Arizona Cardinals', choice_answer = 'Chicago, St. Louis'
+	where question_number = 1 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Sacramento Kings', choice_answer = 'Kansas City'
+	where question_number = 1 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Washington Nationals', choice_answer = 'Montreal'
+	where question_number = 1 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Oakland Athletics', choice_answer = 'Kansas City'
+	where question_number = 1 and choice_id = 'D';
+
+-- Question 3
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '1', choice_answer = '!'
+	where question_number = 3 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '3', choice_answer = '#'
+	where question_number = 3 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '6', choice_answer = '^'
+	where question_number = 3 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '9', choice_answer = '('
+	where question_number = 3 and choice_id = 'D';
+
+-- Question 5
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '(Oregon + California) or France', choice_answer = '262,000 mi2 > 213,000 mi2'
+	where question_number = 5 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '(Germany + Poland + Czech Republic) or Texas', choice_answer = '289,000 mi2 > 268,000 mi2'
+	where question_number = 5 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '(USA + Canada) or Russia', choice_answer = '7.65 million mi2 > 6.6 million mi2'
+	where question_number = 5 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = '(New York (State) + New Jersey + Pennsylvania) or United Kingdom', choice_answer = '109,000 mi2 > 93,000 mi2'
+	where question_number = 5 and choice_id = 'D';
+
+-- Question 7
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Martin Luther King Jr. & Mark Twain', choice_answer = 'False (Born: 1929, Died: 1910)'
+	where question_number = 7 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Mahatma Gandhi & Karl Marx', choice_answer = 'True (Born: 1869, Died: 1883)'
+	where question_number = 7 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'George Washington & Abraham Lincoln', choice_answer = 'False (Died: 1799, Born: 1809)'
+	where question_number = 7 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Billie Eilish & Ronald Reagan', choice_answer = 'True (Born: 2001, Died: 2004)'
+	where question_number = 7 and choice_id = 'D';
+
+-- Question 10
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Population in 1980 is 226.5 million, what is the population in 2020? The average annual population growth rate is 1.13%', choice_answer = '329.5 (313.025, 345.975) Accepted'
+	where question_number = 10 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Population in 1960 is 36.27 million, what is the population in 1970? The average annual population growth rate is 3.86%', choice_answer = '50.29 (47.7755, 52.8045) Accepted'
+	where question_number = 10 and choice_id = 'B';
+
+-- Question 11
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Coca-Cola Company', choice_answer = '4th - 38,655 Million'
+	where question_number = 11 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'CVS Health', choice_answer = '1st - 292,111 Million'
+	where question_number = 11 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Microsoft', choice_answer = '2nd - 198,087 Million'
+	where question_number = 11 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'PepsiCo', choice_answer = '3rd - 79,474 Million'
+	where question_number = 11 and choice_id = 'D';
+
+-- Question 12
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Nike', choice_answer = '4th - 73k'
+	where question_number = 12 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Tesla', choice_answer = '3rd - 99k'
+	where question_number = 12 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'The Walt Disney Company', choice_answer = '2nd - 171k'
+	where question_number = 12 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Wells Fargo', choice_answer = '1st - 247k'
+	where question_number = 12 and choice_id = 'D';
+
+-- Question 13
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'A fictional monkey who is the title character of a series of popular childrens books.', choice_answer = 'Curious George'
+	where question_number = 13 and choice_id = 'A';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Iconic british pop star of the 1980s and 1990s. A hit song of theirs contains the lyric "Guilty feet have got no rhythm", and another hit song of theirs was covered by Limp Bizkit on their debut album.', choice_answer = 'George Michael'
+	where question_number = 13 and choice_id = 'B';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'This actor was in HBOs crime drama The Wire early in his career, before going on to bigger roles, including a part in the Marvel Cinematic Universe. He made his directoral debut in 2023.', choice_answer = 'Michael B. Jordan'
+	where question_number = 13 and choice_id = 'C';
+
+update choices_single_slide_multiple_choice_no_answer_bank
+	set choice_text = 'Very likely the most famous Canadian Psychologist, dead or alive. Most people know him as a podcaster who discusses cultural issues', choice_answer = 'Jordan Peterson'
+	where question_number = 13 and choice_id = 'D';
+
+
+select * from choices_single_slide_multiple_choice_no_answer_bank ssmcnab ;
+
