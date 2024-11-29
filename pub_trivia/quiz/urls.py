f9fb0b2062163827bdda695102c61ceb884f923e
Marley Buchman <michaelmbuchman@gmail.com>
2024-11-29 15:34:20 -0800

    working answer view functionality game wide

--- pub_trivia/quiz/urls.py
+++ pub_trivia/quiz/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
 
-app_name = 'quiz'  # This defines the app namespace for reversing URLs
+app_name = 'quiz'
 
 urlpatterns = [
     path('', views.game_list_view, name='game_list'),  # List available games
-    path('game/<int:game_id>/', views.game_view, name='game_view'),  # View a specific game
+    path('game/<int:game_id>/', views.game_options_view, name='game_options'),  # New Game Options view
+    path('game/<int:game_id>/questions/', views.game_questions_view, name='game_questions'),  # View all questions in a game
+    path('game/<int:game_id>/answers/', views.game_answers_view, name='game_answers'),  # View all answers in a game
     path(
-        'game/<int:game_id>/category/<int:category_id>/question/<int:question_id>/', views.question_view, name='question_view'
+        'game/<int:game_id>/questions/category/<int:category_id>/question/<int:question_id>/', views.question_view, name='question_view'
+    ),
+    path(
+        'game/<int:game_id>/answers/category/<int:category_id>/question/<int:question_id>/', views.answer_view, name='answer_view'
     ),  
 ]
--- pub_trivia/quiz/views.py
+++ pub_trivia/quiz/views.py
@@ -7,24 +7,27 @@ def game_list_view(request):
     games = Game.objects.all()
     return render(request, 'quiz/game_list.html', {'games': games})
 
-def game_view(request, game_id):
-    """
-    View to display all questions in a game, allowing the user to select any question.
-    """
+def game_options_view(request, game_id):
+    """View to let the user choose between viewing questions or answers for a specific game."""
     game = get_object_or_404(Game, id=game_id)
-    
-    # Get all questions in the game, ordered by question_number
-    questions = game.questions.order_by('question_number')
+    return render(request, 'quiz/game_options.html', {'game': game})
 
-    context = {
-        'game': game,
-        'questions': questions,  # Pass all questions to the template
-    }
+def game_questions_view(request, game_id):
+    """View to display all questions in a game."""
+    game = get_object_or_404(Game, id=game_id)
+    questions = game.questions.order_by('question_number')  # Get all questions in order
+    return render(request, 'quiz/game_questions.html', {'game': game, 'questions': questions})
 
-    return render(request, 'quiz/game.html', context)
-
+def game_answers_view(request, game_id):
+    """View to display all answers for a game."""
+    game = get_object_or_404(Game, id=game_id)
+    questions = game.questions.prefetch_related('answers')  # Prefetch answers for efficiency
+    return render(request, 'quiz/game_answers.html', {'game': game, 'questions': questions})
 
 def question_view(request, game_id, category_id, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'quiz/question_view.html', {'question': question})
 
+def answer_view(request, game_id, category_id, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'quiz/answer_view.html', {'question': question})
