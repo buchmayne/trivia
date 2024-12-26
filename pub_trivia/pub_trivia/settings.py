39b4ca18f6c2b0292562cb4c7d53553c769f1b81
Marley Buchman <michaelmbuchman@gmail.com>
2024-12-26 15:55:55 -0500

    postgres migration

--- pub_trivia/pub_trivia/settings.py
+++ pub_trivia/pub_trivia/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "pub_trivia.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pub_trivia_db',
+        'USER': 'pub_trivia_admin',
+        'PASSWORD': 'triviapassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
