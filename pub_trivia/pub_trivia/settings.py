87fafc2689f5607ef9a58ce750f76cd0b371fb9f
Marley Buchman <michaelmbuchman@gmail.com>
2025-01-04 15:53:28 -0800

    updating requirements and adding allowed hosts to settings

--- pub_trivia/pub_trivia/settings.py
+++ pub_trivia/pub_trivia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", 'default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',] # will need to add 'your_domain.com'
 
 
 # Application definition
