242c539a6adc555691cfc583c5d73109ac69d25b
Marley Buchman <michaelmbuchman@gmail.com>
2025-01-14 16:34:01 -0800

    adding security settings

--- pub_trivia/pub_trivia/settings.py
+++ pub_trivia/pub_trivia/settings.py
@@ -139,3 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Security settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
