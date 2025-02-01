905ccb37296b16f9587f741d5a5a30010482e36e
Marley Buchman <michaelmbuchman@gmail.com>
2025-01-31 23:26:07 -0800

    fixed issue with css rendering from static file storage path

--- pub_trivia/pub_trivia/settings.py
+++ pub_trivia/pub_trivia/settings.py
@@ -167,5 +167,4 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_LOCATION = ''
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
