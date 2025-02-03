616f9d9d19379ab1c05d83e012889a62b77eb485
Marley Buchman <michaelmbuchman@gmail.com>
2025-02-02 18:12:23 -0800

    adding cloudfront domain for CDN implementation:

--- pub_trivia/pub_trivia/settings.py
+++ pub_trivia/pub_trivia/settings.py
@@ -165,6 +165,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_CLOUDFRONT_DOMAIN = 'https://d1eomq1h9ixjmb.cloudfront.net'
+AWS_S3_CUSTOM_DOMAIN = AWS_CLOUDFRONT_DOMAIN
 AWS_LOCATION = ''
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
