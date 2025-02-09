5106353ed5e562e36f7b19240e70c6d6a744e73d
Marley Buchman <michaelmbuchman@gmail.com>
2025-02-09 13:08:53 -0800

    adding utils script to load analytics data into app db

---
+++ quiz/utils.py
@@ -0,0 +1,35 @@
+from django.db import transaction
+import pandas as pd
+from typing import Dict, Any
+from quiz.models import GameResult, PlayerStat
+
+
+class AnalyticsLoader:
+    @staticmethod
+    @transaction.atomic
+    def load_game_results(game_results_df: pd.DataFrame) -> None:
+        """
+        Load game results from DataFrame to database
+        """
+        # Convert DataFrame records to list of dicts
+        records = game_results_df.to_dict('records')
+        
+        # Clear existing records and bulk create new ones
+        GameResult.objects.all().delete()
+        GameResult.objects.bulk_create([
+            GameResult(**record) for record in records
+        ])
+    
+    @staticmethod
+    @transaction.atomic
+    def load_player_stats(career_stats_df: pd.DataFrame) -> None:
+        """
+        Load player statistics from DataFrame to database
+        """
+        records = career_stats_df.to_dict('records')
+        
+        # Update or create player stats
+        PlayerStat.objects.all().delete()
+        PlayerStat.objects.bulk_create([
+            PlayerStat(**record) for record in records
+        ])
