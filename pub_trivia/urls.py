e94669ca174445a216f5503aa23b7d4734b10a92
Marley Buchman <michaelmbuchman@gmail.com>
2025-02-16 16:34:47 -0800

    fixing path warning issue

--- pub_trivia/urls.py
+++ pub_trivia/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("quiz/", include("quiz.urls")),
     path("health/", views.health_check, name="health_check"),
     path("", include("quiz.urls")),
 ]
