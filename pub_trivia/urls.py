de666ff0c9bd19ce07797500c37619de2d6f9c92
Marley Buchman <michaelmbuchman@gmail.com>
2025-05-26 15:28:59 -0700

    removing react elements from app. moving forward without it

--- pub_trivia/urls.py
+++ pub_trivia/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 from . import views
 from quiz.views import game_list_view, analytics_view
 
@@ -8,8 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", views.health_check, name="health_check"),
     path("quiz/", include("quiz.urls")),
-    path("new/", TemplateView.as_view(template_name="index.html")),
     path("", game_list_view, name="home"),
     path("analytics/", analytics_view, name="analytics"),
-    re_path(r"^new/.*$", TemplateView.as_view(template_name="index.html")),
 ]
